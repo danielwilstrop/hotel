d2fde9fdda970bdbb307c9e70729c7c06cf3de1b
Daniel Wilstrop <danielwilstrop@daniels-air.home>
2021-01-30 21:54:42 +0000

    services

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import Home from './Pages/Home'
 import Room from './Pages/Room'
 import Error from './Pages/Error'
 import SingleRoom from './Pages/SingleRoom'
+import NavBar from './components/NavBar'
 import './App.css';
 
 function App() {
   return (
   <>
-    <Switch>
-      <Route exact path = '/' component = {Home} />
-      <Route exact path = '/room/' component = {Room} />
-      <Route exact path = '/room/:slug' component = {SingleRoom} />
-      <Route component = {Error} />
-    </Switch>
-      
+    <NavBar />
+      <Switch>
+        <Route exact path = '/' component = {Home} />
+        <Route exact path = '/room/' component = {Room} />
+        <Route exact path = '/room/:slug' component = {SingleRoom} />
+        <Route component = {Error} />
+      </Switch>     
   </>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Hero } from '../components/Hero'
+import  Banner  from '../components/Banner'
+import { Link } from 'react-router-dom'
+import Services from '../components/Services'
 
 export default function Home() {
     return (
-        <div>
-            Home Page
-        </div>
+        <>
+            <Hero>
+                <Banner title = 'Luxurious Rooms' subtitle = 'deluxe rooms starting at £299'>
+                    <Link to = '/room' className = 'btn-primary'>Our Rooms</Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </>
     )
 }
 
